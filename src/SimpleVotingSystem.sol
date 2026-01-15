fa667dc5804f38e389bca406b4dbc76381778602
Nathan Rouleau <nathan7.rouleau@gmail.com>
2026-01-15 13:52:20 +0100

    fix CI and add admin role

--- src/SimpleVotingSystem.sol
+++ src/SimpleVotingSystem.sol
@@ -1,29 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+import {AccessControl} from "openzeppelin-contracts/contracts/access/AccessControl.sol";
 
-contract SimpleVotingSystem  is Ownable{
+contract SimpleVotingSystem is AccessControl {
     struct Candidate {
-        uint id;
+        uint256 id;
         string name;
-        uint voteCount;
+        uint256 voteCount;
     }
 
-    mapping(uint => Candidate) public candidates;
+    mapping(uint256 => Candidate) public candidates;
     mapping(address => bool) public voters;
-    uint[] private candidateIds;
+    uint256[] private candidateIds;
 
-    constructor() Ownable(msg.sender) {}
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
 
-    function addCandidate(string memory _name) public onlyOwner {
+    function addCandidate(string memory _name) public onlyRole(DEFAULT_ADMIN_ROLE) {
         require(bytes(_name).length > 0, "Candidate name cannot be empty");
-        uint candidateId = candidateIds.length + 1;
+        uint256 candidateId = candidateIds.length + 1;
         candidates[candidateId] = Candidate(candidateId, _name, 0);
         candidateIds.push(candidateId);
     }
 
-    function vote(uint _candidateId) public {
+    function vote(uint256 _candidateId) public {
         require(!voters[msg.sender], "You have already voted");
         require(_candidateId > 0 && _candidateId <= candidateIds.length, "Invalid candidate ID");
 
@@ -31,17 +33,17 @@ contract SimpleVotingSystem  is Ownable{
         candidates[_candidateId].voteCount += 1;
     }
 
-    function getTotalVotes(uint _candidateId) public view returns (uint) {
+    function getTotalVotes(uint256 _candidateId) public view returns (uint256) {
         require(_candidateId > 0 && _candidateId <= candidateIds.length, "Invalid candidate ID");
         return candidates[_candidateId].voteCount;
     }
 
-    function getCandidatesCount() public view returns (uint) {
+    function getCandidatesCount() public view returns (uint256) {
         return candidateIds.length;
     }
 
     // Optional: Function to get candidate details by ID
-    function getCandidate(uint _candidateId) public view returns (Candidate memory) {
+    function getCandidate(uint256 _candidateId) public view returns (Candidate memory) {
         require(_candidateId > 0 && _candidateId <= candidateIds.length, "Invalid candidate ID");
         return candidates[_candidateId];
     }
